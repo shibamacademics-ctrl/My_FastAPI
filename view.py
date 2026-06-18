7456b78a065e9e671a420567c1c6253da6a0cbc2
shibamacademics-ctrl <shibamacademics@gmail.com>
2026-06-19 00:46:44 +0530

    First Commit

--- view.py
+++ view.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 import json
+
 def load_data():
-    with open('Patients.tmp','r') as f:
+    with open('patient.json', 'r') as f:
         data = json.load(f)
     return data
+
 app = FastAPI()
+
 @app.get("/")
 def hello():
-    return {"message" : "Patient Mangement System"}
+    return {"message": "Patient Management System"}
+
 @app.get("/about")
 def about():
-    return {"message" : "A fully function API to manage your patient records"}
+    return {"message": "A fully functional API to manage your patient records"}
+
 @app.get("/view")
 def view():
     data = load_data()
